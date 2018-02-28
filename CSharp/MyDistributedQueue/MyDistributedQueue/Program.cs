cba2f48519a7e120fbed58da1da668b1ccc54ac3
zwyang <zwyang@kiloway.com>
2018-02-28 13:33:25 +0800

    上传测试项目

---
+++ CSharp/MyDistributedQueue/MyDistributedQueue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDistributedQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
